975e72ee2ca3ea2430c89ab2409c54072117810e
tackbackward <64315604+tackbackward@users.noreply.github.com>
2020-04-25 15:36:34 +0100

    Added program to run with python3

---
+++ epicprogram.py
@@ -0,0 +1,39 @@
+import PySimpleGUI as sg
+import easygui
+import sys
+from PIL import Image
+
+def rotateimage():
+    image = (fname)
+    msg = "How do I edit this image?"
+    choices = ["Rotate","Flip"]
+    reply = easygui.buttonbox(msg, image=image, choices=choices)
+    if reply == "Rotate":
+        image = Image.open(fname)
+        rotatedimage = image.rotate(90, expand=True)
+        rotatedimage.save(fname)
+        rotateimage()
+
+    if reply == "Flip":
+        image = Image.open(fname)
+        flippedimage = image.transpose(Image.FLIP_LEFT_RIGHT)
+        flippedimage.save(fname)
+        rotateimage()
+
+
+
+if len(sys.argv) == 1:
+    event, values = sg.Window('Rotator',
+                    [[sg.Text('Image to open')],
+                    [sg.In(), sg.FileBrowse()],
+                    [sg.Open(), sg.Cancel()]]).read(close=True)
+    fname = values[0]
+else:
+    fname = sys.argv[1]
+
+if not fname:
+    sg.popup("Cancel", "No filename supplied")
+    raise SystemExit("Cancelling: no filename supplied")
+else:
+
+    rotateimage()
